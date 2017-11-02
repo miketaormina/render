f573e51489a1a693917a3adec1e8c8d0964b895f
Forrest Collman <forrest.collman@gmail.com>
2017-11-02 15:23:53 -0700

    missed import

--- render-ws-java-client/src/main/java/org/janelia/render/client/RenderSectionClient.java
+++ render-ws-java-client/src/main/java/org/janelia/render/client/RenderSectionClient.java
@@ -11,6 +11,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 import org.janelia.alignment.ArgbRenderer;
+import org.janelia.alignment.ShortRenderer;
 import org.janelia.alignment.RenderParameters;
 import org.janelia.alignment.Utils;
 import org.janelia.alignment.spec.Bounds;
